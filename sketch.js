86b2e0d4dab14c6802cf80ff7cbb113bbf3fdfb7
sandra20201982 <70581812+sandra20201982@users.noreply.github.com>
2021-01-10 19:31:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var dog,dogImg,dogImg1;
+var database;
+var foodS,foodStock;
+
+function preload(){
+   dogImg=loadImage("Images/Dog.png");
+   dogImg1=loadImage("Images/happy dog.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(500,500);
+
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale=0.15;
+  ///refering database for value food
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background("orange");
+ 
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
+  }
+
+  drawSprites();
+  fill(255,255,254);
+  stroke("black");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+}
+
+//Function to read values from DB.. 
+function readStock(data){
+  foodS=data.val();
+}
+
+//Function to write values in DB.. 20 will decrease in x=x-1; ..first write condition then update...updatate food value to x
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
+    Food:x
+  })
+}
